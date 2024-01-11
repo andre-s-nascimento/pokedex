63f3279a8cc1bdfa1b4ddb15a37e580ab762751e
Andre S Nascimento <andresoaresnascimento@gmail.com>
2024-01-11 11:47:09 -0300

    Ajuste no CSS

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Projeto Pokedex
+Projeto inspirado no [dribble de Saepul Nahwan]([https://dribbble.com/shots/6540871-Pokedex-App/attachments/6540871-Pokedex-App?mode=media]).
+
 
 ## Tecnologias usadas
 HTML5, CSS, JavaScript
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,29 @@
 
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    // capitalize no JS se quiser receber no objeto
+    // pokemon.name = pokeDetail.name.charAt(0).toUpperCase() + pokeDetail.name.slice(1) 
+    pokemon.name = pokeDetail.name
+    
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-            .then((response) => response.json()) // converte a lista de requisição para json
-
+        .then((response) => response.json()) // converte a lista de requisição para json
+        .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
