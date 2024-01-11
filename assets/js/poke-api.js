dc0e3766943a1dff52ad7300d66253ea472dae1a
Andre S Nascimento <andresoaresnascimento@gmail.com>
2024-01-11 11:06:08 -0300

    Usando os dados da API para mostrar as informações

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,19 @@
 
 const pokeApi = {}
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+            .then((response) => response.json()) // converte a lista de requisição para json
+
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
+    return fetch(url) // buscando a lista de pokemons
+        .then((response) => response.json()) // converte o response para json
+        .then((jsonBody) => jsonBody.results) // pegamos a lista dos resultados
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) // mapeia a lista de pokemons em uma lista de requisições dos detalhes dos pokemons
+        .then((detailRequests) => Promise.all(detailRequests)) // aguarando o processamento async terminar
+        .then((pokemonsDetails) => pokemonsDetails)
         .catch((error) => console.log(error))
 }
